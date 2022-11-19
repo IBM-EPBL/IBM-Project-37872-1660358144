8abff428a0680696dc6e759e10e5076d26602141
Rajavarman1543 <1rajavarman2001@gmail.com>
2022-11-19 17:46:54 +0530

    Application Building

--- Application Building/Build python code/app.py
+++ Application Building/Build python code/app.py
@@ -1,84 +1,91 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
-
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-# Define a flask app
-app = Flask(__name__)
-
-# Model saved with Keras model.save()
-MODEL_PATH = 'model/Mobilenetv2.h5'
-
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
-
-
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
-
-
-@app.route('/', methods=['GET'])
+'''from flask import Flask,request,render_template
+app=Flask(__name__)
+@app.route('/')
+@app.route('/index')
 def index():
-    # Main page
     return render_template('index.html')
+    
+if __name__=="__main__":
+    app.run(debug=True) '''
+    
 
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
-
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-
-        # Make prediction
-        preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
 
 
-if __name__ == '__main__':
+
+
+import requests
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from flask import Flask,request,render_template,redirect,url_for
+import os
+from werkzeug.utils import secure_filename
+from tensorflow.python.keras.backend import set_session
+
+
+
+app=Flask(__name__)
+model=load_model("D:/Rajavarman/Documents/GitHub/IBM-Project-37872-1660358144/Model Building For Vegetable Disease Prediction/Vegetable model/vegetabledata.h5")
+model1=load_model("D:/Rajavarman/Documents/GitHub/IBM-Project-37872-1660358144/Model Building For Fruit Disease Prediction/Test and Save the model/fruitdata.h5")
+
+
+
+@app.route('/')
+def home():
+    return render_template('D:/Rajavarman/Downloads/New folder (2)/IBM-Project-1383-1658386536-main/Application Building/Build HTML code/templates/index.html')
+    
+    
+@app.route('/output')
+def output():
+    return render_template('D:/Rajavarman/Downloads/New folder (2)/IBM-Project-1383-1658386536-main/Application Building/Build HTML code/templatesoutput.html')
+
+@app.route('/prediction')
+def prediction():
+    return render_template('logout.html')
+  
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method=='POST':
+       f=request.files['image']
+       basepath=os.path.dirname(__file__)
+       file_path=os.path.join(basepath,'uploads',secure_filename(f.filename))
+       f.save(file_path)
+       img=image.load_img(file_path,target_size=(128,128))
+       x=image.img_to_array(img)
+       x=np.expand_dims(x,axis=0)
+       plant=request.form['plant']
+       print(plant)
+       if(plant=="vegetable"):
+           preds=model.predict_classes(x)
+           print(preds)
+           df=pd.read_excel('precautions-veg.xlsx')
+           print(df.iloc[preds[0]]['caution'])
+       else:
+           preds=model1.predict_classes(x)
+           
+           df=pd.read_excel('precautions-fruits.xlsx')
+           print(df.iloc[preds[0]]['caution'])
+       return df.iloc[preds[0]]['caution']
+
+
+
+
+if __name__=="__main__":
     app.run(debug=True)
+    
+
+
+
+
+    
+    
+
+    
+    
+    
+    
+ 
+
